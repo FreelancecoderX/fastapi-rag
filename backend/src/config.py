b49873c4eb5f31eab1f8c697967bc435f0e045f1
FreelancecoderX <157281190+FreelancecoderX@users.noreply.github.com>
2024-01-22 23:40:35 +0000

    build index

--- backend/src/config.py
+++ backend/src/config.py
@@ -1,13 +1,22 @@
 from pydantic_settings import BaseSettings
-from pydantic import SecretStr
 
 
-class AppSettings(BaseSettings):
-    ENVIRONMENT: str
-    DB_PASSWORD: SecretStr
+def get_settings():
+    class AppSettings(BaseSettings):
+        ENVIRONMENT: str
+        FILES: str
+        MODEL: str
+        MONGO_URI: str
+        PINECONE_API_KEY: str
+        PINECONE_INDEX: str
+        TOGETHER_API_KEY: str
 
-    class Config:
-        env_file = "env.local"
-        env_file_encoding = "utf-8"
-        secrets_dir = "./secrets"
-        # env_prefix=""
+        class Config:
+            env_file = ".env.local"
+            env_file_encoding = "utf-8"
+
+    settings = AppSettings()
+    return settings
+
+
+settings = get_settings()
